28c8584d4e19a149c4b2d203ca5f8063f6011937
username <abc@gmail.com>
2025-02-22 16:49:34 +0500

    Data ingestion and transformation final

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -1,11 +1,14 @@
 import os
 import sys
-import pandas as pd
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
+import pandas as pd
 from src.mlproject.utils import read_sql_data
-from dataclasses import dataclass
+
 from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+
 
 @dataclass
 class DataIngestionConfig:
@@ -13,33 +16,32 @@ class DataIngestionConfig:
     test_data_path:str=os.path.join('artifacts','test.csv')
     raw_data_path:str=os.path.join('artifacts','raw.csv')
 
-
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
 
-
     def initiate_data_ingestion(self):
         try:
-            ##reading the data code
-            df=read_sql_data()
-            logging.info("Reading data from MYSQL database.")
+            ##reading the data from mysql
+            df=pd.read_csv(os.path.join('nootbook/data','raw.csv'))
+            logging.info("Reading completed mysql database")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Data Ingestion is completed")
 
             return(
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
+                self.ingestion_config.test_data_path
+
+
             )
 
+
         except Exception as e:
-            raise CustomException(e,sys)        
+            raise CustomException(e,sys)
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -5,6 +5,7 @@ import pymysql
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 from dotenv import load_dotenv
+import pickle
 
 
 load_dotenv()
@@ -34,3 +35,16 @@ def read_sql_data():
 
     except Exception as e:
         raise CustomException(e)
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
